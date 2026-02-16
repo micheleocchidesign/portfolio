0b58d642b0ef3e8f9ef4c626baf2cb95ed643e6c
maichi <michele.occhi@gmail.com>
2026-02-16 12:01:15 +0100

    update projects & paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,27 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({  
+export default defineConfig({
+  // L'indirizzo del tuo sito su GitHub Pages
+  site: 'https://ilmaichi.github.io/',
+  
+  // Il nome della tua repository (fondamentale per i percorsi)
+  base: '/mo',
+  
+  // Forza l'aggiunta dello slash finale per evitare errori 404
+  trailingSlash: 'always',
+  
+  build: {
+    // Organizza le pagine in sottocartelle (es. /works/index.html)
+    format: 'directory',
+    // Rinomina la cartella da "_astro" a "assets" per evitare blocchi di GitHub
+    assets: 'assets'
+  },
+  
   vite: {
     server: {
       allowedHosts: [
-        'maegan-uncinctured-untoxically.ngrok-free.dev' // Incolla qui il tuo URL di Ngrok
+        'maegan-uncinctured-untoxically.ngrok-free.dev' // Il tuo URL di Ngrok
       ]
     }
   }
